3209996db69eaf156882c5d534a81c346f0ea38c
XYZ <idm.caz@mailinator.com>
2017-05-19 16:54:35 +0200

    VehicleDataController New test: Returns_valid_data_for_existing_vin_code Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/VehicleDataController.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/VehicleDataController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +20,15 @@ public class VehicleDataController {
     private VehicleDataService vehicleDataService;
 
     @GetMapping("/{vinCode}")
-    public ResponseEntity<VehicleData> getVehicleDataByVinCode(String vinCode) {
+    public ResponseEntity<VehicleData> getVehicleDataByVinCode(@PathVariable String vinCode) {
         try {
-            vehicleDataService.getVehicleData(vinCode);
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            VehicleData vehicleData = vehicleDataService.getVehicleData(vinCode);
+            if (vehicleData == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            else {
+                return new ResponseEntity<>(vehicleData, HttpStatus.OK);
+            }
         }
         catch (Exception e) {
             return new ResponseEntity<VehicleData>(HttpStatus.INTERNAL_SERVER_ERROR);
