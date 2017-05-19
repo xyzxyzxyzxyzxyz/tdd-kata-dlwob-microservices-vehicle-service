989c79e30b86f1e1ce4944678b98df30ce18d56b
XYZ <idm.caz@mailinator.com>
2017-05-19 17:29:16 +0200

    VehicleServiceApplicationTests Test: contextLoads Bugfix: The test was failing because the service implementations MockVehicleServiceImpl and MockVehicleDataServiceImpl lacked the @Service annotation, and Spring wasn't detecting them as beans.

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockVehicleServiceImpl.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockVehicleServiceImpl.java
@@ -1,7 +1,9 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.VehicleInformation;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MockVehicleServiceImpl
         extends AbstractMockServiceImpl<VehicleInformation>
         implements VehicleService
