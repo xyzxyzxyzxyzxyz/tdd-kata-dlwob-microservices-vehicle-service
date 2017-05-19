a2619bb4f6613bafc630c25342d36cbf4c6f419e
XYZ <idm.caz@mailinator.com>
2017-05-19 18:04:23 +0200

    CustomerDataController Test: Returns_404_for_non_existent_customer_id Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/CustomerDataController.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/CustomerDataController.java
@@ -1,6 +1,12 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.controller;
 
+import com.tdd.katas.dlwob.microservices.vehicleservice.model.CustomerData;
+import com.tdd.katas.dlwob.microservices.vehicleservice.service.CustomerDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +16,13 @@ public class CustomerDataController {
 
     public static final String URL_MAPPING = "/customers-data";
 
+    @Autowired
+    private CustomerDataService customerDataService;
+
     @GetMapping("/{customerId}")
-    public void getCustomerDataByCustomerId() {
-        return;
+    public ResponseEntity<CustomerData> getCustomerDataByCustomerId(@PathVariable String customerId) {
+        customerDataService.getCustomerData(customerId);
+        return new ResponseEntity<CustomerData>(HttpStatus.NOT_FOUND);
     }
 
 }
