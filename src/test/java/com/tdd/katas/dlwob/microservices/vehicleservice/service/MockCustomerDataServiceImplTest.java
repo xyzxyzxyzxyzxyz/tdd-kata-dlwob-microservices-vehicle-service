28cc9e88b5201898a07413db443c3c33948d2f95
XYZ <idm.caz@mailinator.com>
2017-05-19 17:41:08 +0200

    MockCustomerDataServiceImpl Test: Contains_sample_data Failing test

--- src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockCustomerDataServiceImplTest.java
+++ src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockCustomerDataServiceImplTest.java
@@ -3,15 +3,13 @@ package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.CustomerData;
 import org.junit.Test;
 
-import java.io.UnsupportedEncodingException;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class MockCustomerDataServiceImplTest {
 
     @Test
-    public void Contains_sample_data() throws UnsupportedEncodingException {
+    public void Contains_sample_data() {
         CustomerDataService mockCustomerDataService = new MockCustomerDataServiceImpl();
 
         CustomerData actualCustomerData = mockCustomerDataService.getCustomerData(MockServicesConstants.SAMPLE_CUSTOMER_ID);
