cb44f0fed0cbb8d2ad40b34dad4faf58850ada39
XYZ <idm.caz@mailinator.com>
2017-05-19 15:56:14 +0200

    VehicleDataController New test: Returns_404_for_non_existent_vin_code Failing test

--- src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/VehicleDataControllerTest.java
+++ src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/VehicleDataControllerTest.java
@@ -1,13 +1,18 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.controller;
 
+import com.tdd.katas.dlwob.microservices.vehicleservice.service.VehicleDataService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,13 +24,25 @@ public class VehicleDataControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private VehicleDataService vehicleDataService;
+
     @Test
     public void Returns_404_for_non_existent_vin_code() throws Exception {
+
+        given(
+                vehicleDataService.getVehicleData( any() )
+            )
+            .willReturn(null);
+
         String NON_EXISTENT_VIN = "NON_EXISTENT_VIN";
 
         mockMvc.perform(
                 get(VehicleDataController.URL_MAPPING + "/{vinCode}", NON_EXISTENT_VIN)
             ).andExpect(status().isNotFound());
+
+        verify(vehicleDataService).getVehicleData( any() );
+
     }
 
 
