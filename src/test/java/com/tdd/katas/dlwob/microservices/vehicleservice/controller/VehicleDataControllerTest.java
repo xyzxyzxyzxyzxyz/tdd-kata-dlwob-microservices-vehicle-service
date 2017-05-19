27d34e9257ca87d6836dbc820a6997d8773d492e
XYZ <idm.caz@mailinator.com>
2017-05-19 16:37:05 +0200

    VehicleDataController New test: Returns_valid_data_for_existing_vin_code Failing test

--- src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/VehicleDataControllerTest.java
+++ src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/VehicleDataControllerTest.java
@@ -1,5 +1,6 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.controller;
 
+import com.tdd.katas.dlwob.microservices.vehicleservice.model.VehicleData;
 import com.tdd.katas.dlwob.microservices.vehicleservice.service.VehicleDataService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,10 +11,12 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -65,6 +68,33 @@ public class VehicleDataControllerTest {
 
     }
 
+    @Test
+    public void Returns_valid_data_for_existing_vin_code() throws Exception {
+        VehicleData expectedVehicleData = new VehicleData();
+        expectedVehicleData.setModelId("test-model-id");
+        expectedVehicleData.setPlateNumber("test-plate-number");
+
+        final String ANY_VIN = "ANY_VIN";
+
+        given ( vehicleDataService.getVehicleData( ANY_VIN ) )
+                .willReturn(expectedVehicleData);
+
+
+
+        mockMvc
+                .perform(
+                        get(VehicleDataController.URL_MAPPING + "/{vinCode}", ANY_VIN)
+                )
+                .andExpect(
+                        status().isOk()
+                )
+                .andExpect( jsonPath("$.modelId", is ( expectedVehicleData.getModelId() ) ))
+                .andExpect( jsonPath("$.plateNumber", is ( expectedVehicleData.getPlateNumber() ) ))
+        ;
+
+        verify (vehicleDataService).getVehicleData(ANY_VIN);
+
+    }
 
 
 }
