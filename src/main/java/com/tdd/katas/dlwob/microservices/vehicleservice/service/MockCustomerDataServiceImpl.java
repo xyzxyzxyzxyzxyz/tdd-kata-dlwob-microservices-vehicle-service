5f28a213a1282f23fc341de800b9a77847b9582c
XYZ <idm.caz@mailinator.com>
2017-05-19 17:47:38 +0200

    MockCustomerDataServiceImpl Test: Contains_sample_data Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockCustomerDataServiceImpl.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockCustomerDataServiceImpl.java
@@ -2,11 +2,23 @@ package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.CustomerData;
 
-public class MockCustomerDataServiceImpl implements CustomerDataService {
+public class MockCustomerDataServiceImpl
+        extends AbstractMockServiceImpl<CustomerData>
+        implements CustomerDataService
+{
+
+    public MockCustomerDataServiceImpl() {
+        super(CustomerData.class);
+    }
 
     @Override
     public CustomerData getCustomerData(String customerId) {
-        return null;
+        if (MockServicesConstants.SAMPLE_CUSTOMER_ID.equals(customerId)) {
+            return dataObject;
+        }
+        else {
+            return null;
+        }
     }
 
 }
