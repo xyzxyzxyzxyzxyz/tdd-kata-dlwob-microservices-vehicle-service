1945eb9e95b16e83442e55be91fafe1e1a171a72
XYZ <idm.caz@mailinator.com>
2017-05-19 12:40:19 +0200

    VehicleController New test: Returns_500_when_service_error Failing test

--- src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/VehicleControllerTest.java
+++ src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/VehicleControllerTest.java
@@ -1,13 +1,17 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.controller;
 
+import com.tdd.katas.dlwob.microservices.vehicleservice.service.VehicleService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,6 +23,9 @@ public class VehicleControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private VehicleService vehicleService;
+
     @Test
     public void Returns_404_for_non_existent_vin_code() throws Exception {
         String NON_EXISTENT_VIN = "NON_EXISTENT_VIN";
@@ -26,6 +33,19 @@ public class VehicleControllerTest {
         mockMvc.perform(
                 get(VehicleController.URL_MAPPING + "/{vinCode}", NON_EXISTENT_VIN)
             ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void Returns_500_when_service_error() throws Exception {
+
+        String ANY_VIN = "ANY_VIN";
+
+        given(vehicleService.getVehicleInformation(any())).willThrow(new RuntimeException("Database is not ready"));
+
+        mockMvc.perform(
+                get(VehicleController.URL_MAPPING + "/{vinCode}", ANY_VIN)
+        ).andExpect(status().isInternalServerError());
+
     }
 
 }
