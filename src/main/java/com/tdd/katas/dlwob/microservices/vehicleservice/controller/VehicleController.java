2beb5a5aa60a643166b7c9c3df50797aa38e539c
XYZ <idm.caz@mailinator.com>
2017-05-19 12:26:55 +0200

    VehicleController New test: Returns_404_for_non_existent_vin_code Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/VehicleController.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/VehicleController.java
@@ -1,5 +1,7 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,8 @@ public class VehicleController {
     public static final String URL_MAPPING = "/vehicles";
 
     @GetMapping("/{vinCode}")
-    public void getVehicle(String vinCode) {
+    public ResponseEntity<?> getVehicle(String vinCode) {
+        return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
     }
 
 }
