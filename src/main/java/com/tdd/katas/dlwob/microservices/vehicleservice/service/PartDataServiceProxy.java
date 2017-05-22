960cf98f01ccd92d0f5fa94186bb43c253b3263c
XYZ <idm.caz@mailinator.com>
2017-05-22 14:56:47 +0200

    PartDataServiceProxy New test: Returns_valid_data_for_existing_vin Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/PartDataServiceProxy.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/PartDataServiceProxy.java
@@ -4,10 +4,12 @@ import com.tdd.katas.dlwob.microservices.vehicleservice.controller.PartDataContr
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.PartData;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -20,8 +22,10 @@ class PartDataServiceProxy {
     }
 
     public List<PartData> getPartDataList(String vinCode) {
+
+        ResponseEntity<PartData[]> response;
         try {
-            restTemplate.getForEntity(PartDataController.URL_MAPPING + "/" + vinCode, PartData[].class);
+            response = restTemplate.getForEntity(PartDataController.URL_MAPPING + "/" + vinCode, PartData[].class);
         }
         catch (HttpClientErrorException e) {
             if (e.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
@@ -32,7 +36,9 @@ class PartDataServiceProxy {
             }
         }
 
-        throw new UnsupportedOperationException("Not implemented");
+        PartData[] partDataArray = response.getBody();
+        return Arrays.asList(partDataArray);
+
     }
 
 }
