ba0250ce8d3c98660f2d8b383237fa2af39783ce
XYZ <idm.caz@mailinator.com>
2017-05-22 11:51:31 +0200

    CustomerDataServiceProxy New test: Returns_null_when_customer_does_not_exist Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/CustomerDataServiceProxy.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/CustomerDataServiceProxy.java
@@ -1,8 +1,11 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
+import com.tdd.katas.dlwob.microservices.vehicleservice.controller.CustomerDataController;
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.CustomerData;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -14,7 +17,16 @@ class CustomerDataServiceProxy {
         restTemplate = restTemplateBuilder.build();
     }
 
-    public CustomerData getCustomerData(String non_existing_customer_id) {
+    public CustomerData getCustomerData(String nonExistingCustomerId) {
+        try {
+            restTemplate.getForEntity(CustomerDataController.URL_MAPPING + "/" + nonExistingCustomerId, CustomerData.class);
+        }
+        catch (HttpClientErrorException e) {
+            if (e.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
+                return null;
+            }
+        }
+
         return null;
     }
 
