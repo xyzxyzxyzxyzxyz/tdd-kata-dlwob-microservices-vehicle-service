dd7e4622d3a4d26a6a1617cc6406efaff884e721
XYZ <idm.caz@mailinator.com>
2017-05-22 15:37:06 +0200

    MockVehicleCustomerServiceImpl New test: Returns_valid_data_for_existing_vin Failing test

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/model/VehicleCustomerData.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/model/VehicleCustomerData.java
@@ -1,4 +1,13 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.model;
 
 public class VehicleCustomerData {
+    private String customerId;
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
 }
--- src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockVehicleCustomerServiceImplTest.java
+++ src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockVehicleCustomerServiceImplTest.java
@@ -1,20 +1,36 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.VehicleCustomerData;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class MockVehicleCustomerServiceImplTest {
 
+    private VehicleCustomerDataService mockVehicleCustomerDataService;
+
+    @Before
+    public void setUp() {
+        mockVehicleCustomerDataService = new MockVehicleCustomerDataServiceImpl();
+    }
+
     @Test
     public void Returns_null_if_vin_does_not_exist() {
-        VehicleCustomerDataService mockVehicleCustomerDataService = new MockVehicleCustomerDataServiceImpl();
-
         final String NON_EXISTING_VIN = "NON_EXISTING_VIN";
         VehicleCustomerData vcd = mockVehicleCustomerDataService.getVehicleCustomerData(NON_EXISTING_VIN);
 
         assertNull(vcd);
+    }
+
+    @Test
+    public void Returns_valid_data_for_existing_vin() {
+        VehicleCustomerData vcd = mockVehicleCustomerDataService.getVehicleCustomerData(MockServicesConstants.SAMPLE_VEHICLE_VIN_CODE);
+
+        assertNotNull(vcd);
+        assertEquals(MockServicesConstants.SAMPLE_CUSTOMER_ID, vcd.getCustomerId());
     }
 
 }
