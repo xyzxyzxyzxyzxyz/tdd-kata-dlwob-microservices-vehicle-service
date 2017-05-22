2ca785754586c8fcbe910c3dcb44c8d060e13896
XYZ <idm.caz@mailinator.com>
2017-05-22 16:13:42 +0200

    CompositeVehicleServiceImpl New test: Returns_null_when_vin_unknown_for_VehicleCustomerDataService Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/CompositeVehicleServiceImpl.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/CompositeVehicleServiceImpl.java
@@ -1,14 +1,26 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
+import com.tdd.katas.dlwob.microservices.vehicleservice.model.VehicleCustomerData;
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.VehicleInformation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompositeVehicleServiceImpl implements VehicleService {
 
+    @Autowired
+    private VehicleCustomerDataService vehicleCustomerDataService;
+
     @Override
     public VehicleInformation getVehicleInformation(String vinCode) {
+
+        VehicleCustomerData vcd = vehicleCustomerDataService.getVehicleCustomerData(vinCode);
+        if (vcd == null) {
+            return null;
+        }
+
         throw new UnsupportedOperationException("To be implemented");
+
     }
 
 }
