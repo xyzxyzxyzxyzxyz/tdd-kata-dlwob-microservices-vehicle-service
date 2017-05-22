bdcdc8175e0f0158aa65757b95cd8d65cebe5534
XYZ <idm.caz@mailinator.com>
2017-05-22 12:33:27 +0200

    VehicleDataServiceProxy New test: Returns_null_when_vin_does_not_exist Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/VehicleDataServiceProxy.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/VehicleDataServiceProxy.java
@@ -1,18 +1,35 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
+import com.tdd.katas.dlwob.microservices.vehicleservice.controller.VehicleDataController;
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.VehicleData;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 class VehicleDataServiceProxy {
 
+    private RestTemplate restTemplate;
+
     public VehicleDataServiceProxy(RestTemplateBuilder restTemplateBuilder) {
-        restTemplateBuilder.build();
+        restTemplate = restTemplateBuilder.build();
     }
 
     public VehicleData getVehicleData(String vinCode) {
-        return null;
+        try {
+            restTemplate.getForEntity(
+                    VehicleDataController.URL_MAPPING + "/" + vinCode,
+                    VehicleData.class);
+        }
+        catch (HttpClientErrorException e) {
+            if (e.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
+                return null;
+            }
+        }
+
+        throw new UnsupportedOperationException("Not implemented");
     }
 
 }
