cc721afdbb6f7e8600c5daa68c44d9e2af055a81
XYZ <idm.caz@mailinator.com>
2017-05-22 09:36:05 +0200

    PartDataController Test: Returns_404_for_non_existent_vin_code Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/PartDataController.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/controller/PartDataController.java
@@ -1,9 +1,16 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.controller;
 
+import com.tdd.katas.dlwob.microservices.vehicleservice.model.PartData;
+import com.tdd.katas.dlwob.microservices.vehicleservice.service.PartDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(PartDataController.URL_MAPPING)
@@ -11,9 +18,13 @@ public class PartDataController {
 
     public static final String URL_MAPPING = "/parts-data";
 
+    @Autowired
+    private PartDataService partDataService;
+
     @GetMapping("/{vinCode}")
-    public void getPartsByVinCode(@PathVariable String vinCode) {
-        return;
+    public ResponseEntity<List<PartData>> getPartsByVinCode(@PathVariable String vinCode) {
+        partDataService.getPartsByVinCode(vinCode);
+        return new ResponseEntity<List<PartData>>(HttpStatus.NOT_FOUND);
     }
 
 }
