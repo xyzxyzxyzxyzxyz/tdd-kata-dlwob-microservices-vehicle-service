9a38c6cd502d9537db58e791668ea019c9b0a3ce
XYZ <idm.caz@mailinator.com>
2017-05-22 10:54:41 +0200

    MockPartDataServiceImplTest Test: Contains_sample_data Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockPartDataServiceImpl.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockPartDataServiceImpl.java
@@ -1,17 +1,27 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.PartData;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class MockPartDataServiceImpl implements PartDataService
+public class MockPartDataServiceImpl
+        extends AbstractMockServiceImpl<List<PartData>>
+        implements PartDataService
 {
+
+    public MockPartDataServiceImpl() {
+        super(
+            new TypeReference<List<PartData>>() {
+            }
+        );
+    }
 
     @Override
     public List<PartData> getPartsByVinCode(String non_existent_vin) {
-        return null;
+        return dataObject;
     }
 
 }
