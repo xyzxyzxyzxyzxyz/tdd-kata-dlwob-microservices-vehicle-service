4e2d044b7f005ec762ec7ef5e77bd297b2c520a9
XYZ <idm.caz@mailinator.com>
2017-05-22 14:35:25 +0200

    PartDataServiceProxy New test: Returns_null_when_vin_does_not_exist Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/PartDataServiceProxy.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/PartDataServiceProxy.java
@@ -1,8 +1,11 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
+import com.tdd.katas.dlwob.microservices.vehicleservice.controller.PartDataController;
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.PartData;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -17,7 +20,16 @@ class PartDataServiceProxy {
     }
 
     public List<PartData> getPartDataList(String vinCode) {
-        return null;
+        try {
+            restTemplate.getForEntity(PartDataController.URL_MAPPING + "/" + vinCode, PartData[].class);
+        }
+        catch (HttpClientErrorException e) {
+            if (e.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
+                return null;
+            }
+        }
+
+        throw new UnsupportedOperationException("Not implemented");
     }
 
 }
