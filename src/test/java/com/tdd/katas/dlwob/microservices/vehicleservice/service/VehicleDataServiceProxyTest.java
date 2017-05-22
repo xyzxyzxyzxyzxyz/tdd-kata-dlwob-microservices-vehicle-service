904e80ca915909b286f7e508781b404664bf167d
XYZ <idm.caz@mailinator.com>
2017-05-22 12:58:01 +0200

    VehicleDataServiceProxy New test: Returns_valid_data_for_existing_vin Failing test

--- src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/VehicleDataServiceProxyTest.java
+++ src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/VehicleDataServiceProxyTest.java
@@ -8,17 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import static org.springframework.test.web.client.ExpectedCount.once;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @RunWith(SpringRunner.class)
 @RestClientTest(VehicleDataServiceProxy.class)
@@ -116,6 +117,47 @@ public class VehicleDataServiceProxyTest {
         catch (HttpServerErrorException e) {
             // Ok
         }
+
+        // Verify expected interaction with server
+        server.verify();
+
+    }
+
+
+    @Test
+    public void Returns_valid_data_for_existing_vin() {
+
+        final String EXISTING_VIN = "ANY_VIN";
+
+        // Prepare server response
+
+        String mockJsonResponse =
+                        "	{														" +
+                        "		\"modelId\": \"sample-vehicle-model-id\",           " +
+                        "		\"plateNumber\": \"sample-vehicle-plate-number\"    " +
+                        "	}                                                       ";
+
+        server
+            .expect(
+                once(),
+                requestTo(VehicleDataController.URL_MAPPING + "/" + EXISTING_VIN)
+            )
+            .andExpect(method(HttpMethod.GET))
+            .andRespond(
+                withSuccess(
+                        mockJsonResponse,
+                        MediaType.APPLICATION_JSON
+                )
+            )
+        ;
+
+        // Execute action
+        VehicleData vehicleData = vehicleDataServiceProxy.getVehicleData(EXISTING_VIN);
+
+        // Verify output
+        assertNotNull(vehicleData);
+        assertEquals("sample-vehicle-model-id", vehicleData.getModelId());
+        assertEquals("sample-vehicle-plate-number", vehicleData.getPlateNumber());
 
         // Verify expected interaction with server
         server.verify();
