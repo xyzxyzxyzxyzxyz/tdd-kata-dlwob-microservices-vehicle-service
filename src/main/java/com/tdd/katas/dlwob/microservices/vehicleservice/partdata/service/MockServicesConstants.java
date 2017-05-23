042d6ab28cdc823365442766ef009e6e09dee6ce
XYZ <idm.caz@mailinator.com>
2017-05-23 10:39:08 +0200

    Refactoring pre - application split : Moving the different microservices to different SpringBoot applications Step #6: Replicating common classes to each microservice - PartData

---
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/partdata/service/MockServicesConstants.java
@@ -0,0 +1,5 @@
+package com.tdd.katas.dlwob.microservices.vehicleservice.partdata.service;
+
+public class MockServicesConstants {
+    public static final String SAMPLE_VEHICLE_VIN_CODE = "sample-vehicle-vin-code";
+}
