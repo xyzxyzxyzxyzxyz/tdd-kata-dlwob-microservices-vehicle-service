5902c301b72173c45adc0ca32398ec91c9349013
XYZ <idm.caz@mailinator.com>
2017-05-23 12:21:45 +0200

    Application split : Moving the different microservices to different SpringBoot applications Step #1: VehicleDataService

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/vehicledata/service/MockServicesConstants.java
+++
@@ -1,5 +0,0 @@
-package com.tdd.katas.dlwob.microservices.vehicleservice.vehicledata.service;
-
-public class MockServicesConstants {
-    public static final String SAMPLE_VEHICLE_VIN_CODE = "sample-vehicle-vin-code";
-}
--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/vehicledata/service/VehicleDataService.java
+++
@@ -1,7 +0,0 @@
-package com.tdd.katas.dlwob.microservices.vehicleservice.vehicledata.service;
-
-import com.tdd.katas.dlwob.microservices.vehicleservice.vehicledata.model.VehicleData;
-
-public interface VehicleDataService {
-    VehicleData getVehicleData(String vinCode);
-}
